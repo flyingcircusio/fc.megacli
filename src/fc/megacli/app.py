fd860c17f7cb7bea174e36da90c783de64c36449
Alexander Bittner <ab@flyingcircus.io>
2016-11-28 11:12:07 +0100

    Bugfixes and simplifications:
    * Fix critical bug in LD to PD mapping. 'diskgroup' is not a valid LD
      identifier. Instead, create mapping beforehand with the help of MegaCli's
      '-LdPdInfo' switch.
    * Be more explicit when searching for LD parameters. Substring matches produces
      false positives.
    * Small typos and table output improvements.
    * Remove unnecessary function definitions.
    * Add user-visible warning about the fact that fc-megacli can currently only
      deal with single controller systems.

--- src/fc/megacli/app.py
+++ src/fc/megacli/app.py
@@ -39,26 +39,28 @@ def summary():
          'LSI\nPD\nStatus']
     ]
 
+    # create LD to PD mapping
+    logical_to_physical = subprocess.check_output([args.megacli_path, '-LdPdInfo', '-aALL'])
+    ld_to_pd = {}
+    for line in logical_to_physical.splitlines():
+        match = re.search('^Virtual Drive: ([0-9][0-9]*).*$|^Enclosure Device ID: ([0-9][0-9]*).*$|^Slot Number: ([0-9][0-9]*).*$', line)
+        if match:
+            if match.group(1):
+                current_ld = match.group(1)
+                if not current_ld in ld_to_pd:
+                    ld_to_pd[current_ld] = []
+            elif match.group(3):
+                ld_to_pd[current_ld].append(match.group(3))
 
-    def search_physicaldrives(physicaldrives, searchstring):
-        drives_found = []
+
+    def get_physicaldrive_by_param(physicaldrives, param, value):
         for drive in physicaldrives:
-            for parameter, value in drive.iteritems():
-                if searchstring in str(value):
-                    drives_found.append(drive)
-        return drives_found
-
-    def search_logicaldrives(logicaldrives, searchstring):
-        lds_found = []
-        for ld in logicaldrives:
-            for parameter, value in ld.iteritems():
-                if searchstring in str(value):
-                    lds_found.append(ld)
-        return lds_found
+            if str(drive[param]) == value:
+                return drive
 
     def get_logicaldrive_param(logicaldevice_id, param):
         for ld in logicaldrives:
-            if logicaldevice_id in str(ld['id']):
+            if logicaldevice_id == str(ld['id']):
                 return ld[param]
 
     def get_mountpoints(device):
@@ -113,7 +115,7 @@ def summary():
            if match:
                logicaldevice_id = match.group(1)
            # get the corresponding Linux device name
-           match = re.search('(sd[a-zA-Z0-9])', line)
+           match = re.search('(sd[a-zA-Z])', line)
            if match:
                device_name = match.group(1)
            # gather data about LD
@@ -121,17 +123,17 @@ def summary():
            ld_state = get_logicaldrive_param(logicaldevice_id, 'state')
            ld_bad_block_exist = get_logicaldrive_param(logicaldevice_id, 'bad_blocks_exist')
            if ld_state.strip() != 'optimal' or ld_bad_block_exist:
-               ld_status = '{}\nBad blocks: {}'.format(ld_state, ld_bad_block_exist)
+               ld_status = 'LD state: {}\nBad blocks: {}'.format(ld_state, ld_bad_block_exist)
            else:
                ld_status = 'OK'
            # avoid duplicates (partitions, respecively. E.g. sda1, sda2, etc.
            # are essentially all sda)
            if not search_nested_structure(table_data, device_name):
-               # loop through all LSI physical devices and pick those that are
-               # part of the current logical device id
-               member_disks = search_physicaldrives(
-                   physicaldrives,
-                   'diskgroup:{}'.format(logicaldevice_id))
+               for slot in ld_to_pd[logicaldevice_id]:
+                   member_disks.append(get_physicaldrive_by_param(
+                       physicaldrives,
+                       'slot_number',
+                       slot))
                for disk in member_disks:
                    member_params += '[{}:{}] {} ({})\n'.format(
                                     disk['enclosure_id'],
@@ -160,5 +162,9 @@ def summary():
                                   ld_status,
                                   member_params, member_status])
 
+    print '\n\n\n=======\nWARNING\n=======\nfc-megacli expects exactly 1 LSI ' \
+          'controller and 1 enclosure!\nIn case you have multiple ' \
+          'controllers and/or enclosures installed,\nDO NOT TRUST fc-megacli ' \
+          'output!\n\n\n'
     table = terminaltables.SingleTable(table_data)
     print table.table
